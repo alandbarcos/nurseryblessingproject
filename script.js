d3486579ecaaeb7ec562619518ecab97d74ffc8c
Alan David Barcos <alandbarcos@gmail.com>
2023-06-02 08:15:41 -0300

    Tercera Entrega - Carrito de Compras - Alan Barcos

--- script.js
+++ script.js
@@ -1,89 +1,136 @@
 
 let subtotal = 0;
 let iva = 0;
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 let producto;
 let busqueda;
 const compras = document.getElementById("productos");
 const verCarrito = document.getElementById("verCarrito");
 const ventanaCarrito = document.getElementById("ventanaCarrito");
 ventanaCarrito.className = "ventanaCarrito";
+const contenedorCarrito = document.createElement("div");
 
-let img1 = document.createElement("img");
-img1.src = "./img/terminados/erizo1.jpeg";
-let img2 = document.createElement("img");
-img2.src = "./img/macetas/maceta-de-barro-clasica.jpg";
-let img3 = document.createElement("img");
-img3.src = "./img/cactus/cactus4.jpg";
-let img4 = document.createElement("img");
-img4.src = "./img/suculentas/suculenta-pequeña.jpg";
-let img5 = document.createElement("img");
-img5.src = "./img/suculentas/suculenta-mediana.jpg";
-let img6 = document.createElement("img");
-img6.src = "./img/terminados/suculenta6.jpeg";
-let img7 = document.createElement("img");
-img7.src = "./img/macetas/casa.jpeg";
-let img8 = document.createElement("img");
-img8.src = "./img/macetas/vaca.jpeg";
-let img9 = document.createElement("img");
-img9.src = "./img/macetas/elefante.jpg";
-let img10 = document.createElement("img");
-img10.src = "./img/terminados/mix2.jpeg";
-
-const prod1 = new CreaProducto(img1,"Maceta de cerámica (Erizo)", 1600 , 130);
-const prod2 = new CreaProducto(img2,"Maceta de barro (Simples)", 700 , 67);
-const prod3 = new CreaProducto(img3,"Cactus pequeño", 230 , 53);
-const prod4 = new CreaProducto(img4,"Suculenta pequeña", 200 , 82);
-const prod5 = new CreaProducto(img5,"Suculenta mediana", 300 , 36);
-const prod6 = new CreaProducto(img6,"Maceta + suculenta/cactus", 2100 , 42);
-const prod7 = new CreaProducto(img7,"Maceta de yeso (Casa)", 1200 , 23);
-const prod8 = new CreaProducto(img8,"Maceta de yeso (Vaca)", 1400 , 15);
-const prod9 = new CreaProducto(img9,"Maceta de cerámica (Elefante)", 900 , 18);
-const prod10 = new CreaProducto(img10,"Maceta grande C/Mix de suculentas", 2500 , 59);
-
-
-
-const Productos = [prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10];
-
-// const servBodas = new CreaServicio("Bodas", 35000);
-// const servCumple = new CreaServicio("Cumpleaños", 22000);
-// const servBautismos = new CreaServicio("Bautismos", 15000);
-// const servAlmuerzosyCenas = new CreaServicio("Almuerzos y Cenas Privadas", 18000);
+const Productos = [{
+    codigo: 1,
+    nombre: "Maceta de cerámica (Erizo)",
+    precio: 1600,
+    stock: 130,
+    imagen: "./img/terminados/erizo1.jpeg",
+},
+{
+    codigo: 2,
+    nombre: "Maceta de barro (Simples)",
+    precio: 700,
+    stock: 67,
+    imagen: "./img/macetas/maceta-de-barro-clasica.jpg",
+},
+{
+    codigo: 3,
+    nombre: "Cactus pequeño",
+    precio: 230,
+    stock: 53,
+    imagen: "./img/cactus/cactus1.jpg",
+},
+{
+    codigo: 4,
+    nombre: "Suculenta pequeña",
+    precio: 200,
+    stock: 82,
+    imagen: "./img/suculentas/suculenta-pequeña.jpg",
+},
+{
+    codigo: 5,
+    nombre: "Suculenta mediana",
+    precio: 300,
+    stock: 36,
+    imagen: "./img/suculentas/suculenta-mediana.jpg",
+},
+{
+    codigo: 6,
+    nombre: "Maceta + suculenta/cactus",
+    precio: 2100,
+    stock: 42,
+    imagen: "./img/terminados/suculenta6.jpeg",
+},
+{
+    codigo: 7,
+    nombre: "Maceta de yeso (Casa)",
+    precio: 1200,
+    stock: 23,
+    imagen: "./img/macetas/casa.jpeg",
+},
+{
+    codigo: 8,
+    nombre: "Maceta de yeso (Vaca)",
+    precio: 1400,
+    stock: 15,
+    imagen: "./img/macetas/vaca.jpeg",
+},
+{
+    codigo: 9,
+    nombre: "Maceta de yeso (Elefante)",
+    precio: 900,
+    stock: 18,
+    imagen: "./img/macetas/elefante.jpg",
+},
+{
+    codigo: 10,
+    nombre: "Maceta grande C/Mix de suculentas",
+    precio: 2500,
+    stock: 59,
+    imagen: "./img/terminados/mix2.jpeg",
+}
+];
 
 Productos.forEach((producto) => {
     const tarjetaProducto = document.createElement("div");
     
     tarjetaProducto.innerHTML = `
+    <img src="${producto.imagen}">
     <h3>${producto.nombre}</h3>
     <p>Precio: $${producto.precio}</p>
     <p>Stock: ${producto.stock}</p>
     `;
-    tarjetaProducto.append(producto.imagen);
+
     compras.append(tarjetaProducto);
     botonCompra = document.createElement("button");
     botonCompra.innerText = "Añadir al carrito";
+    botonCompra.className = "btn btn-primary";
     tarjetaProducto.append(botonCompra);
 
     botonCompra.addEventListener("click", () =>{
         carrito.push({
+            codigo: producto.codigo,
             nombre: producto.nombre,
             precio: producto.precio,
+            stock: producto.stock,
             imagen: producto.imagen,
         });
         console.log(carrito);
+        guardarLocal();
     });
 });
 
 verCarrito.addEventListener("click", () =>{
 
     ventanaCarrito.innerHTML = "";
+    ventanaCarrito.style.display = "block";
+    contenedorCarrito.innerHTML = "";
+    contenedorCarrito.style.display = "block";
 
     const encabezadoCarrito = document.createElement("div");
     encabezadoCarrito.className = "encabezadoCarrito";
     encabezadoCarrito.innerHTML = `
     <h2>Carrito de compras</h2>
-    <button>X</button>
     `;
+
+    const cierreCarrito = document.createElement("button");
+    cierreCarrito.innerText = "X";
+    cierreCarrito.className = "btn btn-danger";
+    cierreCarrito.addEventListener("click",() =>{
+        ventanaCarrito.style.display = "none";
+    })
+    encabezadoCarrito.append(cierreCarrito);
     ventanaCarrito.append(encabezadoCarrito);
 
     carrito.forEach((producto) => {
@@ -92,33 +139,57 @@ verCarrito.addEventListener("click", () =>{
         contenidoCarrito.innerHTML = `
         <h3>${producto.nombre}</h3>
         <h3>Precio: $${producto.precio}</h3>
+        <img src="${producto.imagen}">
         `;
-        contenidoCarrito.append(producto.imagen);
+
         subtotal+=producto.precio;
-        ventanaCarrito.append(contenidoCarrito);
+        iva+=calcularIva(producto.precio);
+        contenedorCarrito.append(contenidoCarrito);
+        ventanaCarrito.append(contenedorCarrito);
     });
 
     const totalCarrito = document.createElement("div");
     totalCarrito.className = "totalCarrito";
     totalCarrito.innerHTML = `
-    <h3>Subtotal: ${subtotal} IVA 21%: ${iva}</h3>`
+    <h3>Subtotal: $${subtotal}</h3>
+    <h3>IVA 21%: $${iva}</h3>
+    <h3>Total a pagar: $${subtotal+iva}</h3>
+    `
     ventanaCarrito.append(totalCarrito);
+    subtotal = 0;
+    iva = 0;
     
+    const operacion = document.createElement("div");
+    operacion.className = "operacion";
+    botonVaciar = document.createElement("button");
+    botonVaciar.className = "btn btn-warning";
+    botonVaciar.innerText = "Vaciar carrito";
+    operacion.append(botonVaciar);
+    ventanaCarrito.append(operacion);
+    botonVaciar.addEventListener("click",() =>{
+        carrito = [];
+        contenedorCarrito.style.display = "none";
+        subtotal = 0;
+        iva = 0;
+        totalCarrito.innerHTML = `
+        <h3>Subtotal: $${subtotal}</h3>
+        <h3>IVA 21%: $${iva}</h3>
+        <h3>Total a pagar: $${subtotal+iva}</h3>
+        `
+        localStorage.removeItem("carrito");
+        console.log(carrito);
+    })
 });
 
-function CreaProducto (imagenProd,nombreProd,precioProd,stockProd){
+function calcularIva(numero){
 
-    this.imagen = imagenProd;
-    this.nombre = nombreProd;
-    this.precio = precioProd;
-    this.stock = stockProd;
+    let resultado = numero*0.21;
+    return resultado;
 }
 
-// function CreaServicio (nombreServ,precioServ){
-
-//     this.nombre = nombreServ;
-//     this.precio = precioServ;
-// }
+const guardarLocal = () =>{
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
 
 
 
